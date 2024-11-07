1570914872a119e2dad60d6c50891a0fc3ac8e9e
Kowsik ND <dkowsikpai@gmail.com>
2024-11-07 12:30:31 +0530

    UDP support and bug fix

--- client.py
+++ client.py
@@ -14,35 +14,59 @@ logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 1024 * 4 #4KB
 
-def send_file(filename, host, port):
+def send_file(filename, host, port, protocol):
     # get the file size
     filesize = os.path.getsize(filename)
     # create the client socket
-    s = socket.socket()
-    logging.info(f"[+] Connecting to {host}:{port}")
-    s.connect((host, port))
-    logging.info("[+] Connected.")
+    if protocol == "TCP":
+        # TCP socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.info(f"[+] Connecting to {host}:{port}")
+        s.connect((host, port))
+        logging.info("[+] Connected.")
+        # send the filename and filesize
+        s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
-    # send the filename and filesize
-    s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+        # start sending the file
+        progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "rb") as f:
+            while True:
+                # read the bytes from the file
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                    # file transmitting is done
+                    break
+                # we use sendall to assure transimission in 
+                # busy networks
+                s.sendall(bytes_read)
+                # update the progress bar
+                progress.update(len(bytes_read))
 
-    # start sending the file
-    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-    with open(filename, "rb") as f:
-        while True:
-            # read the bytes from the file
-            bytes_read = f.read(BUFFER_SIZE)
-            if not bytes_read:
-                # file transmitting is done
-                break
-            # we use sendall to assure transimission in 
-            # busy networks
-            s.sendall(bytes_read)
-            # update the progress bar
-            progress.update(len(bytes_read))
+        # close the socket
+        s.close()
 
-    # close the socket
-    s.close()
+    else:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        addr = (host, port)
+        s.sendto(f"{filename}{SEPARATOR}{filesize}".encode(), addr)
+
+        # start sending the file
+        progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "rb") as f:
+            while True:
+                # read the bytes from the file
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                    # file transmitting is done
+                    break
+                # we use sendall to assure transimission in 
+                # busy networks
+                s.sendto(bytes_read, addr)
+                # update the progress bar
+                progress.update(len(bytes_read))
+                
+        # close the socket
+        s.close()
 
 if __name__ == "__main__":
     import argparse
@@ -50,8 +74,10 @@ if __name__ == "__main__":
     parser.add_argument("--file", help="File name to send")
     parser.add_argument("--host", help="The host/IP address of the receiver", default="127.0.0.1")
     parser.add_argument("-p", "--port", help="Port to use, default is 5001", type=int, default=5001)
+    parser.add_argument("--protocol", type=str, default="TCP", choices=["TCP", "UDP"], help="Server Protocol")
     args = parser.parse_args()
+
     filename = args.file
     host = args.host
     port = args.port
-    send_file(filename, host, port)
+    send_file(filename, host, port, args.protocol)
--- server.py
+++ server.py
@@ -15,6 +15,7 @@ logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 parser = argparse.ArgumentParser("Client Program")
 parser.add_argument("--host", type=str, default="127.0.0.1", help="Server address")
 parser.add_argument("--port", type=int, default=8000, help="Server port")
+parser.add_argument("--protocol", type=str, default="TCP", choices=["TCP", "UDP"], help="Server Protocol")
 
 args = parser.parse_args()
 
@@ -25,12 +26,9 @@ SERVER_PORT = args.port
 BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
 
-def handler(socket):
-    # accept connection if there is any
-    client_socket, address = socket.accept() 
+def tcp_handler(client_socket, address):
     # if below code is executed, that means the sender is connected
     logging.info(f"[+] {address} is connected.")
-
     # receive the file infos
     # receive using client socket, not server socket
     received = client_socket.recv(BUFFER_SIZE).decode()
@@ -54,9 +52,37 @@ def handler(socket):
             f.write(bytes_read)
             # update the progress bar
             progress.update(len(bytes_read))
+        f.close()
 
     # close the client socket
     client_socket.close()
+
+
+def udp_handler(client_socket):
+    received, s_addr = client_socket.recvfrom(BUFFER_SIZE)
+    received = received.decode()
+    filename, filesize = received.split(SEPARATOR)
+    # remove absolute path if there is
+    filename = os.path.basename(filename)
+    # convert to integer
+    filesize = int(filesize)
+    # start receiving the file from the socket
+    # and writing to the file stream
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(f"server_{filename}", "wb") as f:
+        while True:
+            # read 1024 bytes from the socket (receive)
+            bytes_read, s_addr = client_socket.recvfrom(BUFFER_SIZE)
+            if not bytes_read:    
+                # nothing is received
+                # file transmitting is done
+                break
+            # write to the file the bytes we just received
+            f.write(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+        f.close()
+
 
 
 if __name__ == "__main__":
@@ -65,21 +91,32 @@ if __name__ == "__main__":
     thread_connections = []
 
     # create the server socket
-    # TCP socket
-    s = socket.socket()
+    if args.protocol == "TCP":
+        # TCP socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    else:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     # bind the socket to our local address
     s.bind((SERVER_HOST, SERVER_PORT))
     # enabling our server to accept connections
     # 5 here is the number of unaccepted connections that
     # the system will allow before refusing new connections
-    s.listen(5)
+    if args.protocol == "TCP":
+        s.listen(5)
     logging.info(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
     # Handle new connections
-    thd = threading.Thread(target=handler, args=(s,))
-    thread_connections.append(thd)
-    thd.start()
-    # handler(s)
-
-    # close the server socket
-    s.close()
+    # accept connection if there is any
+    if args.protocol == "TCP":
+        while True:
+            client_socket, address = s.accept() 
+            thd = threading.Thread(target=tcp_handler, args=(client_socket, address))
+            thread_connections.append(thd)
+            thd.start()
+            # handler(s)
+        # s.close() # Wait for the next client; Remove While true and uncomment for single client
+    else:
+        thd = threading.Thread(target=udp_handler, args=(s,))
+        thread_connections.append(thd)
+        thd.start()
